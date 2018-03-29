54d1f681e8b66f40e9e0e6cfa85daf030dc81076
bstriker <strikeforcezero@gmail.com>
2018-03-29 12:38:35 -0400

    Add pjsua2_net_test project

---
+++ pjsua2_net_test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using PJSIP;
+
+namespace pjsua2_net_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var endpoint = new PJSIP.Endpoint();
+            endpoint.libCreate();
+
+            endpoint.libInit(new EpConfig
+            {
+                uaConfig = { maxCalls = 6 },
+                medConfig =
+                {
+                    sndClockRate = 16000,
+                    //noVad = true,
+                    ecTailLen = 0,
+                    hasIoqueue = true
+                }
+            });
+            
+            var randomPort = new Random().Next(1025, 65534);
+            endpoint.transportCreate(pjsip_transport_type_e.PJSIP_TRANSPORT_UDP, new TransportConfig() { port = (uint)randomPort });
+
+            endpoint.libStart();
+
+            endpoint.libRegisterThread(Thread.CurrentThread.Name ?? "");
+
+            Console.ReadKey();
+        }
+    }
+}
